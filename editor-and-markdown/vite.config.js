06950d96c95bb97882653a349dd2e305158ca82a
yaoshitingopen <128033573+yaoshitingopen@users.noreply.github.com>
2024-03-14 15:50:44 +0800

    Add files via upload

---
+++ editor-and-markdown/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': resolve('src')
+    }
+  },
+})
